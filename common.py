c94da1a93906ddcab4398f190fb8a86389cd9bfb
Davide Corradini <updates+github.com@corradinidavi.de>
2024-12-08 11:26:11 +0000

    Updated stdout prints

--- common.py
+++ common.py
@@ -15,11 +15,14 @@ CODE_COVERAGE_PATH = '/code-coverage'
 # Print welcoe ASCII art
 def welcome():
     print("    ____  _________________                    \n   / __ \\/ ____/ ___/_  __/___ ___  ______ ___ \n  / /_/ / __/  \\__ \\ / / / __ `/ / / / __ `__ \\\n / _, _/ /___ ___/ // / / /_/ / /_/ / / / / / /\n/_/ |_/_____//____//_/  \\__, /\\__, /_/ /_/ /_/ \n                       /____//____/            ")
-    #print(" ____              _____ _____ _____ _____ \n|    \\ ___ ___ ___| __  |   __|   __|_   _|\n|  |  | -_| -_| . |    -|   __|__   | | |  \n|____/|___|___|  _|__|__|_____|_____| |_|  \n              |_|                          ")
     print("Welcome to the RESTgym experiment infrastructure.")
 
 def get_apis():
-    return os.listdir('./apis')
+    apis = os.listdir('./apis')
+    apis.remove('#api-template')
+    return apis
 
 def get_tools():
-    return os.listdir('./tools')
+    tools = os.listdir('./tools')
+    tools.remove('#tool-template')
+    return tools
